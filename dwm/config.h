b5a74c21eded06db496d1183d8c1202a68c40147
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2024-09-06 18:36:11 -0300

    auto

--- dwm/config.h
+++ dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -105,13 +105,14 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} }, // Layout button
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} }, // Resize window with right click
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} }, // Move window with left click
 //	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 //	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 //	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-//	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 //	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-//	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+
 //	{ ClkTagBar,            0,              Button1,        view,           {0} },
 //	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 //	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
