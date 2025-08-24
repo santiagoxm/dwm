576bc8b8b2de0f716c40322e9e1a00bfe6450add
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-08-24 19:37:01 -0300

    changed border colors

--- dwm/config.h
+++ dwm/config.h
@@ -18,13 +18,12 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_gray5[]       = "#666666";
 static const char col_black[]       = "#000000";
 static const char col_white[]       = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_black, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1,  col_gray5  },
+	[SchemeNorm] = { col_gray3, col_black, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_gray3  },
 };
 
 /* tagging */
